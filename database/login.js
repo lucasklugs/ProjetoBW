b9292ad2dea2a22669e85c11964f7bfd3aa4e02e
Lucas Klug Sebastião <lucasklugs@gmail.com>
2023-07-02 16:34:02 -0300

    comments

--- database/login.js
+++ database/login.js
@@ -1,5 +1,7 @@
-const{ default: mongoose, Schema, model} = require("mongoose");
+// Importando biblioteca mongoose
+const{ default: mongoose, Schema, model} = require("mongoose"); 
 
+// Definindo um novo esquema (Schema)
 const login = new Schema({
     username: {type: String},
     email: {type: String},
@@ -7,5 +9,6 @@ const login = new Schema({
     
 })
 
-const login_model = new model("logins", login)
+// criando modelo login_model de acordo com o schema acima e exportando para ser utilizado em arquivos (require)
+const login_model = new model("logins", login) 
 module.exports = login_model
