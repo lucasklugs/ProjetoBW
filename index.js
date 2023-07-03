240305e34606973a106f17676aba1409bc76509b
Lucas Klug Sebastião <118365668+lucasklugs@users.noreply.github.com>
2023-07-03 09:40:44 -0300

    welcome alert 2.0

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(session({
      if (req.body.password === result.password){
         req.session.type = "user" 
          console.log("logged in")
-         res.redirect('/?li=true');
+         res.redirect('/index.html?li=true'); // volta para homepage e efetua um alerta
 
      }  
      else {
--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,3 +1,4 @@
+// faz um alerta na página index se usúario estiver logado
 if (window.location.href.includes("li=true")){
     window.alert("bem vindo")
 }
